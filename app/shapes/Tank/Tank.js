0b01d600be82b2eaa3cd9cd57c19431a3ac2cbb8
Ryan Hansberry <rhansby@gmail.com>
2013-05-22 11:30:29 -0700

    Implemented the TankTurret class. Tanks now have turrets on top of them: http://puu.sh/2YUNL.png

--- app/shapes/Tank/Tank.js
+++ app/shapes/Tank/Tank.js
@@ -1,33 +1,41 @@
 function Tank(offset, y_rotation) {
-    
+
     var body = new TankBody(offset, y_rotation);
-    var turret;
-    
+    var turret = new TankTurret({x: offset.x, y: offset.y + 0.5, z: offset.z}, y_rotation);
+
+    var setTurretOffset = function() {
+        turret.offset = {x: body.offset.x, y: body.offset.y + 0.5, z: body.offset.z};
+    };
+
     this.getOffset = function() {
         return body.offset;
     };
-    
+
     this.getBodyYRotation = function() {
         return body.rotation.y;
     };
-    
+
     this.moveOnXAxis = function(units) {
         body.moveOnXAxis(units);
+        setTurretOffset();
     };
-    
+
     this.moveOnZAxis = function(units) {
         body.moveOnZAxis(units);
+        setTurretOffset();
     };
-    
+
     this.rotateOnYAxis = function(units) {
         body.rotateOnYAxis(units);
     };
-    
+
     this.update = function() {
         body.update();
+        turret.update();
     };
-    
+
     this.draw = function() {
         body.draw();
+        turret.draw();
     };
 }
---
+++ app/shapes/Tank/TankTurret.js
@@ -0,0 +1,128 @@
+function TankTurret(offset, y_rotation) {
+    // Create an array of vertices for the cube.
+    var vertices = [
+        // Front face
+        -0.5,    0,  0.5,
+         0.5,    0,  0.5,
+         0.5,  0.5,  0.5,
+        -0.5,  0.5,  0.5,
+
+        // Back face
+        -0.5,    0, -0.5,
+        -0.5,  0.5, -0.5,
+         0.5,  0.5, -0.5,
+         0.5,    0, -0.5,
+
+        // Top face
+        -0.5,  0.5, -0.5,
+        -0.5,  0.5,  0.5,
+         0.5,  0.5,  0.5,
+         0.5,  0.5, -0.5,
+
+        // Bottom face
+        -0.5,    0, -0.5,
+         0.5,    0, -0.5,
+         0.5,    0,  0.5,
+        -0.5,    0,  0.5,
+
+        // Right face
+         0.5,    0, -0.5,
+         0.5,  0.5, -0.5,
+         0.5,  0.5,  0.5,
+         0.5,    0,  0.5,
+
+        // Left face
+        -0.5,    0, -0.5,
+        -0.5,    0,  0.5,
+        -0.5,  0.5,  0.5,
+        -0.5,  0.5, -0.5
+    ];
+
+    // Normals:
+    var normals = [
+        // Front
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+        0.0,  0.0,  1.0,
+
+        // Back
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+        0.0,  0.0, -1.0,
+
+        // Top
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+        0.0,  1.0,  0.0,
+
+        // Bottom
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+        0.0, -1.0,  0.0,
+
+        // Right
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+        1.0,  0.0,  0.0,
+
+        // Left
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0
+    ];
+
+    var colors = [];
+    for (var i = 0; i < 24; i++) {
+        colors = colors.concat([0.4,  0.4,  0.4,  1.0]);
+    }
+
+    // This array defines each face as two triangles, using the
+    // indices into the vertex array to specify each triangle's position.
+    var vertex_indices = [
+        0,  1,  2,      0,  2,  3,  // front
+        4,  5,  6,      4,  6,  7,  // back
+        8,  9,  10,     8,  10, 11, // top
+        12, 13, 14,     12, 14, 15, // bottom
+        16, 17, 18,     16, 18, 19, // right
+        20, 21, 22,     20, 22, 23  // left
+    ];
+
+    Shape.call(this, vertices, normals, colors, vertex_indices); // inherit from Shape
+
+    this.offset = offset || {x: 0, y: 0, z: 0};
+    this.rotation = {x: 0, y: y_rotation, z: 0};
+    this.scale = {x: 1, y: 1, z: 1};
+
+    this.rotateOnYAxis = function(units) {
+        this.rotation.y -= units;
+
+        if ( this.rotation.y > 360.0 ) {
+            this.rotation.y -= 360.0;
+        }
+        if ( this.rotation.y < -360.0 ) {
+            this.rotation.y += 360.0;
+        }
+    };
+}
+
+inheritPrototype(TankTurret, Shape);
+
+// Overloading Shape.prototype's update():
+// (Rotation comes after translation, and only rotating around Y axis)
+TankTurret.prototype.update = function() {
+    mvPushMatrix();
+
+    mvTranslate([this.offset.x, this.offset.y, this.offset.z]);
+    mvRotate(this.rotation.y, [0, 1, 0]);
+    mvScale(this.scale.x, this.scale.y, this.scale.z);
+
+    setMatrixUniforms();
+
+    mvPopMatrix();
+};
