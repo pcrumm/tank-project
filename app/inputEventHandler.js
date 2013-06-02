0a7345358d41da160caa87879384f2911bf26170
Ryan Hansberry <rhansby@gmail.com>
2013-06-02 02:47:50 -0700

    Removed mouse control and updated readme

--- app/inputEventHandler.js
+++ app/inputEventHandler.js
@@ -1,26 +1,10 @@
 var currentlyPressedKeys = {};
-var window_center_horizontal = document.body.clientWidth / 2;
-var window_in_focus = true;
 
 function initInputEventHandler() {
     document.onkeydown = handleKeyDown;
     document.onkeyup = handleKeyUp;
-    document.onmousedown = handleMouseDown;
-    document.onmouseup = handleMouseUp;
-    document.onmousemove = handleMouseMove;
-
-    // If the window goes out of focus, turn off mouse capture, and vice versa:
-    window.onblur = function() { window_in_focus = false; };
-    window.onfocus = function() { window_in_focus = true; };
-
-    window.onmouseout = function() { window_in_focus = false; };
-    window.onmouseover = function() { window_in_focus = true; };
-
-    // Fix the center if the window is resized:
-    window.onresize = function() { window_center_horizontal = document.body.clientWidth / 2; };
 
     setInterval(handleKeys, 30);
-    setInterval(handleMouse, 30);
 }
 
 function handleKeyDown(event) {
@@ -70,54 +54,4 @@ function handleKeys() {
         var player_tank = player.getTank();
         multiplayer.sendTankUpdate(player_tank.id, player_tank.getOffset(), player_tank.getBodyYRotation());
     };
-}
-
-var mouseInfo = {
-    threshold_left:  function() { return window_center_horizontal - 50 },
-    threshold_right: function() { return window_center_horizontal + 50 },
-
-    looking_left:  false,
-    looking_right: false,
-
-    rotation_magnitude_horizontal: 0,
-    rotation_magnitude_divisor: 100
-};
-
-function handleMouseDown(event) {
-    if (event.which == 1) // left click
-        player.shootOn();
-}
-
-function handleMouseUp(event) {
-    if (event.which == 1) // left click
-        player.shootOff();
-}
-
-function handleMouseMove(event) {
-    // Handle horizontal mouse movement:
-    if ( event.pageX < mouseInfo.threshold_left() ) {
-        mouseInfo.looking_left  = true;
-        mouseInfo.looking_right = false;
-        mouseInfo.rotation_magnitude_horizontal = mouseInfo.threshold_left() - event.pageX;
-    }
-    else if ( event.pageX > mouseInfo.threshold_right() ) {
-        mouseInfo.looking_left  = false;
-        mouseInfo.looking_right = true;
-        mouseInfo.rotation_magnitude_horizontal = event.pageX - mouseInfo.threshold_right();
-    }
-    else {
-        mouseInfo.looking_left  = false;
-        mouseInfo.looking_right = false;
-    }
-}
-
-function handleMouse() {
-    if ( window_in_focus ) {
-        if ( mouseInfo.looking_left ) {
-            player.rotateTankTurretLeft(mouseInfo.rotation_magnitude_horizontal / mouseInfo.rotation_magnitude_divisor);
-        }
-        else if ( mouseInfo.looking_right ) {
-            player.rotateTankTurretRight(mouseInfo.rotation_magnitude_horizontal / mouseInfo.rotation_magnitude_divisor);
-        }
-    }
 }
