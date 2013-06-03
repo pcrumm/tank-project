92118a5bf6f98eabf5d5b8b428cc4dd42bc82285
Kasey Carrothers <kaseyc.808@gmail.com>
2013-06-03 16:00:20 -0700

    Particles stay above terrain
    The function now makes sure that each particles velocity will not send
    it under the terrain.
    
    This is done by checking that the angle between the normal at that point
    and the velocity is less than pi/2 (90 degrees)

--- app/shapes/emitter.js
+++ app/shapes/emitter.js
@@ -31,6 +31,47 @@ function copy(obj) {
     }
 }
 
+function getNormal(x, z) {
+    var x1, z1;
+
+    if (x > .01)
+        x1 = x - .01;
+
+    else
+        x1 = x + .01;
+
+    if (z > .01)
+        z = z - .01;
+
+    else
+        z = z + .01;
+
+    var y = terrain.getMapHeightAndSlope(x, z);
+    var y1 = terrain.getMapHeightAndSlope(x1, z);
+    var y2 = terrain.getMapHeightAndSlope(x, z1);
+
+    var curr = $V([x, y, z]);
+    var c1   = $V([x1, y1, z]);
+    var c2   = $V([x, y2, z1]);
+
+    var normal =  (c1.subtract(curr)).cross(c2.subtract(curr));
+
+    normal = normal.toUnitVector();
+
+    return {x: normal.x, y: normal.y, z: normal.z};
+}
+
+function angleBetween(vec1, vec2) {
+    //Returns the angle between 2 vectors in radians
+    var dot = (vec1.x * vec2.x) + (vec1.y * vec2.y) + (vec1.z * vec2.z);
+
+    var mag1 = Math.sqrt((vec1.x * vec1.x) + (vec1.y * vec1.y) + (vec1.z * vec1.z));
+
+    var mag2 = Math.sqrt((vec2.x * vec2.x) + (vec2.y * vec2.y) + (vec2.z * vec2.z));
+
+    return Math.acos(dot / (mag1 * mag2));
+} 
+
 function Emitter(offset, rotation, num_particles, acceleration, maxSpeed) {
     this.offset = offset || {x: 0, y: 0, z: 0};
 
@@ -44,15 +85,20 @@ function Emitter(offset, rotation, num_particles, acceleration, maxSpeed) {
     var fade;
     var speed = maxSpeed || 1;
     var vX, vY, vZ;
+    var angle;
 
     for (var i = 0; i < num_particles; i++) {
         fade = getRandInRange(0, 99)/1000 + .03;
 
         //Set a velocity that does not go into the terrain
         //A small value is added to make sure each particle has some speed
+        do {
         vX = getRandInRange(0, speed) - (.5 * speed) + .005;
         vY = getRandInRange(0, speed) - (.5 * speed) + .005;
         vZ = getRandInRange(0, speed) - (.5 * speed) + .005;
+
+        angle = angleBetween( {x: vX, y: vY, z: vZ}, getNormal(this.offset.x, this.offset.z) );
+        } while (angle > Math.PI);
 
         this.particles.push(new Particle(copy(this.offset), {x: vX, y: vY, z: vZ}, pull, fade));
     }
